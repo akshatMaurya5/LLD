5b646a6f335c932328a463b9d80d1613aac644b7
Akshat Maurya <mauryakshat5@gmail.com>
2025-03-11 22:09:33 +0530

    final ones

--- final_int_practice/5_poll_system/main.cpp
+++ final_int_practice/5_poll_system/main.cpp
@@ -143,11 +143,11 @@ public:
 
 int main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("error.txt", "w", stderr);
-    freopen("output.txt", "w", stdout);
-#endif
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("error.txt", "w", stderr);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
 
     PollManager pollManager;
 
